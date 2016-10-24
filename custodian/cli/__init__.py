9d8b48697934e8698a017641870045f506384c21
__sp__ <ongsp@eng.ucsd.edu>
2016-10-23 19:31:28 -0700

    Custodiandoc.

--- custodian/cli/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-# Copyright (c) Materials Virtual Lab.
-# Distributed under the terms of the BSD License.
-
-from __future__ import division, unicode_literals, print_function
-
-"""
-#TODO: Replace with proper module doc.
-"""
